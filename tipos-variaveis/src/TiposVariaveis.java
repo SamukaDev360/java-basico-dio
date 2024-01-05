d4f5580a05e49c1c239e1e08101100dd027baad9
samuel <saamuka70@gmail.com>
2024-01-04 23:22:22 -0300

    att

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,9 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
         System.out.println("Hello,World!");
+        int valorVariavel = 2;
+
+        System.out.println(valorVariavel);
 
     }
 }
