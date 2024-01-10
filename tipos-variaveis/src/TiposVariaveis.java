494bc5e0a770247db98eb0a791ea6f6fa56baab7
samuel <saamuka70@gmail.com>
2024-01-09 22:27:36 -0300

    att

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,9 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello,World!");
-        int valorVariavel = 2;
+        // DOUBLE OBSERVAÇÃO:
+        // A respeito do tipo double o "." ele nao quer dizer milhar ou seja se eu digitar 2.500
+        // achando que sera representado o valor de 2500 esta na verdade representando 2.5
+       // double salarioMinimo = 2500;
+        //System.out.println(salarioMinimo);
 
-        System.out.println(valorVariavel);
+        // OBSERVAÇÃO DE UM TIPO PARA OUTRO UTILIZANDO CAST.
+        //int pode ser maior do que um short, e a perda de precisão pode ocorrer
+        // se o valor atribuído for maior do que o limite do short
+        //short numeroCurto = 1;
+        //int numeroNormal = numeroCurto;
+        //short numeroCurto2 = (short) numeroNormal;
+
+        final double VALOR_PI = 3.14;
+        System.out.println(VALOR_PI);
+
+
 
     }
 }
