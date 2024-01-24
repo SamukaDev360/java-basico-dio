5de3b104db3ecc60af3a55dd2ea3386ecbdbb4bc
samuel <saamuka70@gmail.com>
2024-01-23 22:21:43 -0300

    aulaatt

---
+++ java-terminal/src/MyClassScanner.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MyClassScanner {
+    public static void main(String[] args) {
+        // criando objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobreNome = scanner.next();
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+
+        // imprimindo
+        System.out.println("Olá, me chamo: " + nome + " " + sobreNome);
+        System.out.println("Tenho: " + idade + " anos ");
+        System.out.println("Minha altura é: " + altura + "cm");
+
+    }
+}
