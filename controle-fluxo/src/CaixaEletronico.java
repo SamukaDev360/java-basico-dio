619599573509223f0a8542204d891c33618e8ec6
Samuel <saamuka70@gmail.com>
2024-02-05 22:39:55 -0300

    atualizando

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,8 +1,13 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
-        double saldo = 25.0;
-        double valorSolicitado = 17;
 
+        // declarando variaveis saldo e valor solicitado
+        double saldo = 25.0;
+        double valorSolicitado = 26;
+
+
+      /* verificando se valor solicitado é menor que saldo, se for iremos realizar a operação dentro
+         na nossa condicional descontando o valor se disponivel, caso nosso saldo for insuficiente ele ira mostrar apenas nosso saldo */
         if (valorSolicitado < saldo)
             saldo = saldo - valorSolicitado;
 
---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,16 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        // declarando variavel nota
+        int nota = 8;
+
+        // condicional composta onde caso temos mais de uma jornada em nosso programa caso a nota seja maior ou igual a 7.
+        if (nota >= 7) {
+            System.out.println("Aprovado");
+        } else {
+            System.out.println("Reprovado");
+        }
+        
+        
+        
+    }
+}
