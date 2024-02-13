2271da0c904a336d80230264936ab647a8bc8723
samuel <saamuka70@gmail.com>
2024-02-13 20:06:06 -0300

    att

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -5,15 +5,12 @@ public class CaixaEletronico {
         double saldo = 25.0;
         double valorSolicitado = 26;
 
-
       /* verificando se valor solicitado é menor que saldo, se for iremos realizar a operação dentro
          na nossa condicional descontando o valor se disponivel, caso nosso saldo for insuficiente ele ira mostrar apenas nosso saldo */
         if (valorSolicitado < saldo)
             saldo = saldo - valorSolicitado;
 
-
         System.out.println(saldo);
-
 
     }
 }
